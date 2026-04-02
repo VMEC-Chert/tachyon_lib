37f796096cee8d70f924e072e51b20f035144b0c
Mallchad <ncaprisunfan@gmail.com>
2026-04-02 01:22:56 +0100

    Fixed: Issue with iffy error check on limit function

--- source/tachyon_safety.h
+++ source/tachyon_safety.h
@@ -10,7 +10,7 @@ PROC limit( T arg )
 {   T low_clamp = (arg > 0 ? arg : 0 );
     T type_max = std::numeric_limits<t_return>::max();
     T high_low_clamp = (low_clamp < type_max ? low_clamp : type_max);
-    if (high_low_clamp != arg)
+    if (arg < 0 || arg > type_max)
     {   TYON_ERRORF( "Arg {} exceeds range of return type", arg ); }
     return static_cast<t_return>( high_low_clamp );
 
